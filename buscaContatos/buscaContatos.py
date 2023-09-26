c5245374a3f0ebccf685e35750ffc66a81319c26
Otavio Prada <142634808+OtavioPrada10@users.noreply.github.com>
2023-09-26 16:57:55 -0300

    Update buscaContatos.py

--- buscaContatos/buscaContatos.py
+++ buscaContatos/buscaContatos.py
@@ -2,28 +2,27 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 class BuscaContatos:
     def __init__(self):
-        self.SITE_LINK = "https://www.linkedin.com/feed/"
-        self.SITE_MAP = {
-            "Login":{
-                "Email": {
-                    "xpath":"/html/body/div[1]/main/div/div/form/div[1]/div[1]/div/div/input"
-                }
-            }
-        }
+        self.SITE_LINK = "https://www.linkedin.com/"
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.maximize_window()
     def abrir_site(self):
         time.sleep(2)
         self.driver.get(self.SITE_LINK)
-        time.sleep(10)
+        time.sleep(4)
         
     def loga(self):
-        self.driver.find_element(self.SITE_MAP["Login"]["Email"]["xpath"]).click()
-        time.sleep(10)
+        usuario_input = self.driver.find_element('name', 'session_key')
+        usuario_input.send_keys("xxxxxx@xxxxxx.com")
+
+        senha_input = self.driver.find_element('name', 'session_password')
+        senha_input.send_keys("XXXXXXXXXXXX")
+        time.sleep(5)
         
 
 
