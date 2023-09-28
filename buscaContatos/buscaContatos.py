2768f9151bab248cc961a130d1a80157ffc6d973
Otavio Prada <142634808+OtavioPrada10@users.noreply.github.com>
2023-09-28 17:50:14 -0300

    Update buscaContatos.py

--- buscaContatos/buscaContatos.py
+++ buscaContatos/buscaContatos.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from login import Login
 from selenium.webdriver.common.by import By
 
 
@@ -11,21 +12,32 @@ class BuscaContatos:
         self.SITE_LINK = "https://www.linkedin.com/"
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.maximize_window()
-    def abrir_site(self):
-        time.sleep(2)
+    '''
+    Função responsavel por entrar no site solicitado neste contexto o Linkedin
+    :param self
+    '''
+    def abrirSite(self):
+        time.sleep(1)
         self.driver.get(self.SITE_LINK)
-        time.sleep(4)
-        
+        time.sleep(1)
+    '''
+    Função responsavel por efetuar o login
+    '''
     def loga(self):
         usuario_input = self.driver.find_element('name', 'session_key')
-        usuario_input.send_keys("xxxxxx@xxxxxx.com")
+        usuario_input.send_keys(Login.EMAIL)
 
         senha_input = self.driver.find_element('name', 'session_password')
-        senha_input.send_keys("XXXXXXXXXXXX")
-        time.sleep(5)
+        senha_input.send_keys(Login.SENHA)
         senha_input.send_keys(Keys.RETURN)
-
-
-Contato = BuscaContatos()
-Contato.abrir_site()
-Contato.loga()
+    '''
+    Função responsavel por fazer a busca pelo nome da empresa
+    :param Nome da empresa
+    '''
+    def buscaEmpresa(self, nomeEmpresa):
+        #Busca o elemnto da barra de pesqisa
+        barraPesquisa = self.driver.find_element('css selector', '#global-nav-typeahead > input')
+        barraPesquisa.send_keys(nomeEmpresa)
+        barraPesquisa.send_keys(Keys.RETURN)
+        filtroEmpresa = self.driver.find_element('css selector', '#search-reusables__filters-bar > ul > li:nth-child(2) > button')
+        filtroEmpresa.click()
