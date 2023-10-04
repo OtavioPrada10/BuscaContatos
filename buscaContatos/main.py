0414c068daf23e2895ce9779aa33ebd0dc8f9784
Otavio Prada <142634808+OtavioPrada10@users.noreply.github.com>
2023-10-04 15:58:46 -0300

    Revisão do codigo

--- buscaContatos/main.py
+++ buscaContatos/main.py
@@ -10,8 +10,8 @@ sg.theme('DarkBlue')   # Add a touch of color
 # All the stuff inside your window.
 layout = [  [sg.Text('Facilitador de pesquisa de Contas', size = 30)],
             [sg.Text('nome da empresa', size = 14), sg.InputText(key='nomeEmpresa')],
-            [sg.Text('Cargo',         size = 14), sg.InputText(key='cargo')],
-            [sg.Text('Cidade' ,           size = 14), sg.InputText(key='cidade')],
+            [sg.Text('Cargo',           size = 14), sg.InputText(key='cargo')],
+            [sg.Text('Cidade' ,         size = 14), sg.InputText(key='cidade')],
             [sg.Button('Ok'), sg.Button('Cancel')]]
 
 window = sg.Window('Facilitador', layout)
@@ -21,7 +21,6 @@ while True:
         inicio = 1
         Contato = BuscaContatos()
         Contato.abrirSite()
-        # Contato.loga()
 
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':
